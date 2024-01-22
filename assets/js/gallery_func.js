ebce496973c9f97a70bd026b23e09f9cc49feaa1
PhunksTechnologies <justadmin@rambler.ru>
2024-01-22 07:55:13 +0400

    css edits

--- assets/js/gallery_func.js
+++ assets/js/gallery_func.js
@@ -1,11 +1,13 @@
 function enlarge (element){
     // element.style.maxHeight = '20em';
-    element.style.transform = 'scale(4.1)';
+    // element.style.transform = 'scale(4.1)';
+    element.style.transform = 'scale(3.6) translate(0, -30%)';
     element.style.zIndex = 1000;
 }
 
 function shrink (element){
     // element.style.maxHeight = '10em';
-    element.style.transform = 'scale(1)';
+    // element.style.transform = 'scale(1)';
+    element.style.transform = 'scale(1) translate(0, 0)';
     element.style.zIndex = 1;
 }
