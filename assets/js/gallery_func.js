e8333513e03fdc3e89838c4f3c5e2490c764d6a1
PhunksTechnologies <justadmin@rambler.ru>
2024-01-22 07:47:12 +0400

    added images gallery

---
+++ assets/js/gallery_func.js
@@ -0,0 +1,11 @@
+function enlarge (element){
+    // element.style.maxHeight = '20em';
+    element.style.transform = 'scale(4.1)';
+    element.style.zIndex = 1000;
+}
+
+function shrink (element){
+    // element.style.maxHeight = '10em';
+    element.style.transform = 'scale(1)';
+    element.style.zIndex = 1;
+}
