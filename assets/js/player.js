c0ea319bef03150f3561b6e6eef63f83d5123bad
PhunksTechnologies <justadmin@rambler.ru>
2024-01-23 02:27:11 +0400

    added footer

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,17 +4,26 @@ const tracks = [
    "http://dl.dropbox.com/scl/fi/hswjnjd32em0acvp11gtl/Hvat-2023.08.05-No-Lag.mp3?rlkey=gn89w0lg8j9mjqtfbhfsevkby&dl=0", 
    "http://dl.dropbox.com/scl/fi/s6usmueayhlx2hm49a89b/Phunky-Promo-2023-07-30.mp3?rlkey=cphgirgq0izsssex94fgl0emn&dl=0", 
    "radio"
-]
+];
+
 function switch_track(track_el, number) {
     document.getElementById("audio_src").src = tracks[number];
     Array.from(document.getElementsByClassName('track')).forEach(el => el.classList.remove('nowplaying'));
     track_el.classList.add('nowplaying');
     document.getElementById("audio").load(); //call this to just preload the audio without playing
     document.getElementById("audio").play(); //call this to play the song right away
-}
+};
 
-
-{/* <p class="track" onclick="switch_track(this, 1)">Basslogik live @Hvat-2023.08.05</p>
-<p class="track" onclick="switch_track(this, 2)">No Lag live @Hvat-2023.08.05</p>
-<p class="track" onclick="switch_track(this, 3)">Phunky Promo 202.07.30</p>
-<p class="track" onclick="switch_track(this, 4)">DNB Radio</p> */}
+document.onload = function (){
+    document.querySelector('#audio').addEventListener("ended", (event) => {
+        tracks.forEach(track => {
+            let next_track;
+            if (tracks.indexOf(track) == tracks.length-1) {
+                next_track = 0;
+            } else{
+                next_track = tracks.indexOf(track)+1;
+                if (audio.src == track) switch_track(document.querySelectorAll('.playlist:nth-child(' + next_track + ')'), next_track);
+            }
+        });
+    });
+};
